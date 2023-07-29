e8eb14da3ed6e281c4438fff61e6e900984d03f1
Mustapha Tijani <thenewxpat@gmail.com>
2023-07-29 16:29:48 +0100

    notification in progress

--- app/Notifications/ExpiringProductNotification.php
+++ app/Notifications/ExpiringProductNotification.php
@@ -6,15 +6,17 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use Illuminate\Support\HtmlString;
 
-class ExpiringProductNotification extends Notification
+
+class ExpiringProductNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
     /**
      * Create a new notification instance.
      */
-    public function __construct()
+    public function __construct(protected array $data)
     {
         //
     }
@@ -34,21 +36,38 @@ class ExpiringProductNotification extends Notification
      */
     public function toMail(object $notifiable): MailMessage
     {
+        $content = "<ul>";
+        foreach ($this->data['stocks'] as $stock) {
+            $content.= $stock->product?->name . " with batch number $stock->batch_no purchased on $stock->purchase date will expire on $stock->created_at. There are $stock->remaining_quantity left";
+        }
+
+        $content .= "</ul>";
+
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+                    ->greeting('Hello Jendol,')
+                    ->line(new HtmlString($content))
+                    ->line($this->data['message'])
+                    ->action('Go to dashboard', route('dashboard'))
+                    ->line('Thumps up!');
     }
 
-    /**
-     * Get the array representation of the notification.
-     *
-     * @return array<string, mixed>
-     */
+
     public function toArray(object $notifiable): array
     {
         return [
-            //
+            'data' => json_encode($this->data)
         ];
+    }
+
+    public function toVonage(object $notifiable): VonageMessage
+    {
+        $content = "";
+        foreach ($this->data['stocks'] as $stock) {
+            $content.= $stock->product?->name.", ";
+        }
+
+        $content .= "</ul>";
+        return (new VonageMessage)
+                    ->content($data['message'].". $content");
     }
 }
